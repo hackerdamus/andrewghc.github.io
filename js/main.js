97fca20259107bdc3a9cef36ed960cfbaacd195e
AndrewGHC <drewwalsh1@gmail.com>
2016-04-21 10:18:56 +0200

    Improved navbar, added form validation & general improvement overall

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
+    var noRepeat = true;
     $('#fullpage').fullpage({
-        //Navigation
+        // Navigation options for fullpage.js
         menu: '#menu',
-        lockAnchors: false,
         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        navigation: false,
-        navigationPosition: 'right',
         navigationTooltips: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        showActiveTooltip: false,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-
+        // Handle events when moving between slides
         onLeave: function(index, nextIndex, direction) {
+            // Animate slide arrows to make it more obvious the page can be scrolled left & right
+            if (nextIndex === 3 && noRepeat) {
+                noRepeat = false;
+                $('.navbar-brand-header').notify('Click the arrows or use your left and right arrow keys to scroll this page!', {
+                    className: "success",
+                    position: "right center"
+                });
+                $('.fp-controlArrow').addClass('animated zoomInUp');
+                setTimeout(function() {
+                    $('.fp-controlArrow').removeClass('zoomInUp');
+                    $('.fp-controlArrow').addClass('shake');
+                }, 1000);
+            }
+            // Change navbar text colour on first and other slides
             if (nextIndex === 1) {
                 $('a').attr('style', 'color:white !important');
             } else {
@@ -21,6 +30,7 @@ $(document).ready(function() {
     });
 });
 
+// Revert navbar to default when toggle is clicked to improve legibility
 $('.navbar-toggle').on('click', function() {
     if ($(this).hasClass('collapsed')) {
         $('nav').removeClass('navbar-custom');
